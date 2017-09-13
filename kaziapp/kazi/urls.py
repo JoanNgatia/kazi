e424b2c30d7193101b59c247238eb8ede6a0c1bb
JoanNgatia <joan.ngatia@andela.com>
2017-09-13 20:57:48 +0300

    Refactor code with modelviewsets

--- kaziapp/kazi/urls.py
+++ kaziapp/kazi/urls.py
@@ -1,15 +1,30 @@
-from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
-import views
+# from django.conf.urls import url
+# from rest_framework.urlpatterns import format_suffix_patterns
+# import views
 
-urlpatterns = {
-    url(r'^employers/$', views.EmployersView.as_view(), name='all_employers'),
-    url(r'^employers/(?P<pk>[0-9]+)/$',
-        views.EmployersDetailView.as_view(), name='single_employer'),
-    url(r'^employers/(?P<employer_id>[0-9]+)/employees/$',
-        views.EmployeesView.as_view(), name='all_employees'),
-    url(r'^employers/(?P<employer_id>[0-9]+)/employees/(?P<pk>[0-9]+)/$',
-        views.EmployeeDetailView.as_view(), name='single_employee'),
-}
+# Method One of routing the modelviewset
+# urlpatterns = {
+#     url(r'^employers/$', views.EmployersView.as_view(
+#         {'get': 'list', 'post': 'create'}), name='all_employers'),
+#     url(r'^employers/(?P<pk>[0-9]+)/$',
+#         views.EmployersView.as_view({'get': 'retrieve', 'put': 'update'}),
+#         name='single_employer'),
+#     url(r'^employers/(?P<employer_id>[0-9]+)/employees/$',
+#         views.EmployeesView.as_view({'get': 'list', 'post': 'create'}),
+#         name='all_employees'),
+#     url(r'^employers/(?P<employer_id>[0-9]+)/employees/(?P<pk>[0-9]+)/$',
+#         views.EmployeesView.as_view({'get': 'retrieve', 'put': 'update'}),
+#         name='single_employee'),
+# }
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = format_suffix_patterns(urlpatterns)
+
+from rest_framework.routers import SimpleRouter
+
+from .views import EmployeesView, EmployersView
+
+router = SimpleRouter()
+router.register("employers", EmployersView)
+router.register("employees", EmployeesView)
+
+urlpatterns = router.urls
--- kaziapp/kazi/views.py
+++ kaziapp/kazi/views.py
@@ -1,41 +1,50 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from rest_framework import generics
+from rest_framework import viewsets
 
 from models import Employer, Employee
 from serializers import EmployerSerializer, EmployeeSerializer
 
 
-class EmployersView(generics.ListCreateAPIView):
-    """Add and view all employers."""
+# class EmployersView(generics.ListCreateAPIView):
+#     """Add and view all employers."""
 
+#     serializer_class = EmployerSerializer
+#     queryset = Employer.objects.all()
+
+
+# class EmployersDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     """View, update or delete single employer."""
+
+#     serializer_class = EmployerSerializer
+#     queryset = Employer.objects.all()
+
+
+# class EmployeesView(generics.ListCreateAPIView):
+#     """Add and view all employees."""
+
+#     serializer_class = EmployeeSerializer
+
+#     def get_queryset(self):
+#         pk = self.kwargs.get('employer_id')
+#         return Employee.objects.filter(employer=pk)
+
+
+# class EmployeeDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     """Single employee Retrieve update and delete."""
+
+#     serializer_class = EmployeeSerializer
+
+#     def get_queryset(self):
+#         employer_id = self.kwargs.get('employer_id')
+#         return Employee.objects.filter(employer=employer_id)
+
+class EmployersView(viewsets.ModelViewSet):
     serializer_class = EmployerSerializer
     queryset = Employer.objects.all()
 
 
-class EmployersDetailView(generics.RetrieveUpdateDestroyAPIView):
-    """View, update or delete single employer."""
-
-    serializer_class = EmployerSerializer
-    queryset = Employer.objects.all()
-
-
-class EmployeesView(generics.ListCreateAPIView):
-    """Add and view all employees."""
-
+class EmployeesView(viewsets.ModelViewSet):
     serializer_class = EmployeeSerializer
-
-    def get_queryset(self):
-        pk = self.kwargs.get('employer_id')
-        return Employee.objects.filter(employer=pk)
-
-
-class EmployeeDetailView(generics.RetrieveUpdateDestroyAPIView):
-    """Single employee Retrieve update and delete."""
-
-    serializer_class = EmployeeSerializer
-
-    def get_queryset(self):
-        employer_id = self.kwargs.get('employer_id')
-        return Employee.objects.filter(employer=employer_id)
+    queryset = Employee.objects.all()
