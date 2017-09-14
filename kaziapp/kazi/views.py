290a0f858362f60befc8fa708edcd3d6cda59294
JoanNgatia <joan.ngatia@andela.com>
2017-09-14 13:20:50 +0300

    Remove commented out code

--- kaziapp/kazi/views.py
+++ kaziapp/kazi/views.py
@@ -7,39 +7,6 @@ from models import Employer, Employee
 from serializers import EmployerSerializer, EmployeeSerializer
 
 
-# class EmployersView(generics.ListCreateAPIView):
-#     """Add and view all employers."""
-
-#     serializer_class = EmployerSerializer
-#     queryset = Employer.objects.all()
-
-
-# class EmployersDetailView(generics.RetrieveUpdateDestroyAPIView):
-#     """View, update or delete single employer."""
-
-#     serializer_class = EmployerSerializer
-#     queryset = Employer.objects.all()
-
-
-# class EmployeesView(generics.ListCreateAPIView):
-#     """Add and view all employees."""
-
-#     serializer_class = EmployeeSerializer
-
-#     def get_queryset(self):
-#         pk = self.kwargs.get('employer_id')
-#         return Employee.objects.filter(employer=pk)
-
-
-# class EmployeeDetailView(generics.RetrieveUpdateDestroyAPIView):
-#     """Single employee Retrieve update and delete."""
-
-#     serializer_class = EmployeeSerializer
-
-#     def get_queryset(self):
-#         employer_id = self.kwargs.get('employer_id')
-#         return Employee.objects.filter(employer=employer_id)
-
 class EmployersView(viewsets.ModelViewSet):
     serializer_class = EmployerSerializer
     queryset = Employer.objects.all()
